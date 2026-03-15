e8b30e0d9f5843df099ef83aa280f4d960a0edc8
imustbenewperson-png <imustbenewperson@gmail.com>
2026-03-15 23:51:29 +0300

    Update main.py

--- main.py
+++ main.py
@@ -93,7 +93,7 @@ def assemblyai_transcribe_with_speakers(audio_path, num_speakers):
         "audio_url": audio_url,
         "speaker_labels": True,
         "speakers_expected": num_speakers,
-        "speech_model": "best",
+        "speech_models": ["best"],
         "punctuate": True,
         "format_text": True
     }
